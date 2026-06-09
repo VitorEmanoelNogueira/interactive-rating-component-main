1089d0096e176297d35cc18b506749ccdf41ce26
Vitor Emanoel da Silva Nogueira <vitoremanoelnogueira@gmail.com>
2026-06-09 13:02:43 -0300

    fix: improve accessibility of rating state transition

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 const ratingComponent = document.querySelector('.c-rating');
 const ratingForm = ratingComponent.querySelector('.c-rating__form');
+const thankYouTitle = ratingComponent.querySelector('.js-thank-you-title');
 const selectedRatingDisplay = ratingComponent.querySelector('.js-selected-rating');
+
+ratingForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let selectedRating = getSelectedRating();
+    showThankYouMessage(selectedRating);
+})
 
 function getSelectedRating(){
     return ratingForm.querySelector('.c-rating__option:checked').value;
 }
 
 function showThankYouMessage(selectedRating){
-    ratingComponent.classList.add('is-rated');
     selectedRatingDisplay.textContent = selectedRating;
+    ratingComponent.classList.add('is-rated');
+    thankYouTitle.focus();
 }
 
-ratingForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    let selectedRating = getSelectedRating();
-    console.log(selectedRating)
-    showThankYouMessage(selectedRating);
-})
